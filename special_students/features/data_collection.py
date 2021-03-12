c05e46ae2863832c5cbed2c6d8227ec7af252397
Ale Farias <mail.alefarias@gmail.com>
2021-03-12 20:35:16 -0300

    Adds collection of concentration area and courses data

--- special_students/features/data_collection.py
+++ special_students/features/data_collection.py
@@ -1,4 +1,33 @@
+import json
+
 import pandas as pd
 
 from special_students.scraping.spiders import (SpiderGetConcentrationAreaData,
                                                SpiderGetCourseData)
+
+
+def get_courses():
+    df = pd.read_csv("data/processed/alunos_cursos.csv")
+    return df["disciplina"].drop_duplicates().values
+
+
+def generate_courses_data(courses):
+    spider = SpiderGetCourseData()
+    data = []
+    for course in courses:
+        data.append(spider.collect(course))
+
+    with open("data/processed/cursos.json", "w") as course_json:
+        course_json.write(json.dumps(data))
+
+
+def generate_concentration_area_data():
+    spider = SpiderGetConcentrationAreaData()
+    with open("data/processed/areas_concentracao.json", "w") as concentrarion_area_json:
+        concentrarion_area_json.write(json.dumps(spider.collect()))
+
+
+if __name__ == "__main__":
+    courses = get_courses()
+    generate_courses_data(courses)
+    generate_concentration_area_data()
