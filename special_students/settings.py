14d5c90c0638f1b7bb1a2b7c0dbbdc7f945cf0ce
Ale Farias <mail.alefarias@gmail.com>
2021-03-17 23:21:12 -0300

    pep8 fixes

--- special_students/settings.py
+++ special_students/settings.py
@@ -1,4 +1,5 @@
 from typing import List
+
 from pydantic import BaseSettings
 
 
@@ -6,12 +7,8 @@ class Settings(BaseSettings):
     raw_pdf_path: str = "data/raw/ResultadoAE20211.pdf"
     interim_students_path: str = "data/interim/alunos.csv"
     interim_students_courses_path: str = "data/interim/alunos_cursos.csv"
-    
-    courses_columns: List[str] = [
-        "disciplina_1",
-        "disciplina_2",
-        "disciplina_3"
-    ]
+
+    courses_columns: List[str] = ["disciplina_1", "disciplina_2", "disciplina_3"]
 
 
 settings = Settings()
